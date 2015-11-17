0f255b26d4c4d5fd45a9b2b9ad00578755a5e966
jcsantamaria <jcsgit13>
2015-11-17 15:52:09 -0500

    - Update with latest base class ClockBotHardware - Add squelch

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/GripCalibrate.java
@@ -0,0 +1,94 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * OpMode to calibrate gripper.
+ */
+public class GripCalibrate extends ClockBotHardware {
+
+    final double SQUELCH_DURATION = 0.5;
+
+    double leftOpenPosition;
+    double leftClosePosition;
+    double rightOpenPosition;
+    double rightClosePosition;
+    StopWatch squelch;
+
+    @Override
+    public void start() {
+        // set default target position
+        leftOpenPosition = 0.0;
+        leftClosePosition = 0.5;
+        rightOpenPosition = 0.0;
+        rightClosePosition = 0.5;
+
+        squelch = new StopWatch();
+
+        telemetry.addData("left", leftOpenPosition + " " + leftClosePosition);
+    }
+
+    @Override
+    public void loop() {
+        // arm control
+        if(gamepad1.dpad_up && squelch.elapsedTime() > SQUELCH_DURATION) {
+            leftOpenPosition += 0.01;
+            Range.clip(leftOpenPosition, 0, 1);
+            squelch.reset();
+        }
+        else if(gamepad1.dpad_down && squelch.elapsedTime() > SQUELCH_DURATION) {
+            leftOpenPosition -= 0.01;
+            Range.clip(leftOpenPosition, 0, 1);
+            squelch.reset();
+        }
+        if(gamepad1.dpad_right && squelch.elapsedTime() > SQUELCH_DURATION) {
+            leftClosePosition += 0.01;
+            Range.clip(leftClosePosition, 0, 1);
+            squelch.reset();
+        }
+        else if(gamepad1.dpad_left && squelch.elapsedTime() > SQUELCH_DURATION) {
+            leftClosePosition -= 0.01;
+            Range.clip(leftClosePosition, 0, 1);
+            squelch.reset();
+        }
+        else if (gamepad1.y) {
+
+            leftGrip.setPosition(leftOpenPosition);
+        }
+        else if (gamepad1.x)
+        {
+            leftGrip.setPosition(leftClosePosition);
+        }
+        telemetry.addData("left", leftOpenPosition + " " + leftClosePosition);
+        //---------------------------------------------------------------------------------------------------
+        if(gamepad1.dpad_up) {
+            rightOpenPosition += 0.01;
+            Range.clip(rightOpenPosition, 0, 1);
+        }
+        else if(gamepad1.dpad_down) {
+            rightOpenPosition -= 0.01;
+            Range.clip(rightOpenPosition, 0, 1);
+        }
+        if(gamepad1.dpad_right) {
+            rightClosePosition += 0.01;
+        }
+        else if(gamepad1.dpad_left) {
+            rightClosePosition -= 0.01;
+        }
+        else if (gamepad1.b) {
+
+            rightGrip.setPosition(rightOpenPosition);
+        }
+        else if (gamepad1.a)
+        {
+            rightGrip.setPosition(rightClosePosition);
+        }
+        telemetry.addData("right", rightOpenPosition + " " + rightClosePosition);
+    }
+}
+
+
