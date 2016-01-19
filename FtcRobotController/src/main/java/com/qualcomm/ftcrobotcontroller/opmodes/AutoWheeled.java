89986b13c51b1dea520e1a768516ac5a3b81c444
jcsantamaria <jcsgit13>
2016-01-19 18:14:05 -0500

    Improve control

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutoWheeled.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutoWheeled.java
@@ -12,8 +12,9 @@ public class AutoWheeled extends WheeledBotHardware {
     final double ARM_LOWER_POWER =-0.02;
     final double ARM_RAISE_POWER = 0.08;
     final int    ARM_SET_POSITION = 820;
-    final double DRIVE_POWER = 0.08;
-
+    final double DRIVE_POWER = 0.12;
+    final double WAYPOINT_THRESHOLD = 300;
+    final double SQUELCH_DURATION = 0.5;
     enum RobotState
     {
         LowerArm,
@@ -24,11 +25,11 @@ public class AutoWheeled extends WheeledBotHardware {
         Stop
     }
 
-    double targetx1 = 0;
-    double targety1 = 2200;
-    double targetx2 = 2200;
-    double targety2 = 2200;
-
+    double targetx1 = -2500;
+    double targety1 = 4200;
+    double targetx2 = -5000;
+    double targety2 = 5000;
+    double drive_power = DRIVE_POWER;
     RobotState state;
     StopWatch squelch;
 
@@ -94,18 +95,29 @@ public class AutoWheeled extends WheeledBotHardware {
 
                 // normalize
                 double norm = Math.sqrt(dx * dx + dy * dy);
-                dx = dx / norm;
-                dy = dy / norm;
+                double angle = Math.atan2(dy,dx);
+                //dx = dx / norm;
+                //dy = dy / norm;
+
+                double mag = 1;
+                if ( norm < WAYPOINT_THRESHOLD * 2) {
+                    mag = norm / (WAYPOINT_THRESHOLD * 2);
+                }
+
+                dx = mag * Math.sin(angle - heading);
+                dy = mag * Math.cos(angle - heading);
 
                 //Calculate the power needed for each motor
                 double leftPower  = dy + dx;
                 double rightPower = dy - dx;
 
                 //set value to be less so it's not going crazy
-                double magnitude = DRIVE_POWER;
+                double magnitude = drive_power;
                 setDrivePower(magnitude * leftPower, magnitude * rightPower);
 
-               if (norm < 100) {
+                telemetry.addData("control", String.format("%.0f %.0f %.0f %.2f %.2f", heading * RAD2DEG, angle * RAD2DEG, (angle - heading) * RAD2DEG, dx, dy));
+
+               if (norm < WAYPOINT_THRESHOLD) {
                     state = RobotState.Waypoint2;
                }
             }
@@ -114,23 +126,34 @@ public class AutoWheeled extends WheeledBotHardware {
             case Waypoint2:
             {
                 // compute delta vector
-                double dx = targetx1 - positionX;
-                double dy = targety1 - positionY;
+                double dx = targetx2 - positionX;
+                double dy = targety2 - positionY;
 
                 // normalize
                 double norm = Math.sqrt(dx * dx + dy * dy);
-                dx = dx / norm;
-                dy = dy / norm;
+                double angle = Math.atan2(dy,dx);
+                //dx = dx / norm;
+                //dy = dy / norm;
+
+                double mag = 1;
+                if ( norm < WAYPOINT_THRESHOLD * 2) {
+                    mag = norm / (WAYPOINT_THRESHOLD * 2);
+                }
+
+                dx = mag * Math.sin(angle - heading);
+                dy = mag * Math.cos(angle - heading);
 
                 //Calculate the power needed for each motor
                 double leftPower  = dy + dx;
                 double rightPower = dy - dx;
 
                 //set value to be less so it's not going crazy
-                double magnitude = DRIVE_POWER;
+                double magnitude = drive_power;
                 setDrivePower(magnitude * leftPower, magnitude * rightPower);
 
-                if (norm < 100) {
+                telemetry.addData("control", String.format("%.0f %.0f %.0f %.2f %.2f", heading * RAD2DEG, angle * RAD2DEG, (angle - heading) * RAD2DEG, dx, dy));
+
+                if (norm < WAYPOINT_THRESHOLD) {
                     state = RobotState.Stop;
                 }
             }
@@ -144,8 +167,19 @@ public class AutoWheeled extends WheeledBotHardware {
             break;
         }
 
+
+        // adjust the move forward duration using the up/down dpad
+        if ( gamepad1.dpad_down && squelch.elapsedTime() > SQUELCH_DURATION) {
+            drive_power = Math.max(drive_power - 0.01, 0.1);
+            squelch.reset();
+        }
+        if ( gamepad1.dpad_up && squelch.elapsedTime() > SQUELCH_DURATION) {
+            drive_power = Math.min(drive_power + 0.01, 2 * DRIVE_POWER);
+            squelch.reset();
+        }
         telemetry.addData("state", state.toString());
-        telemetry.addData("pos", String.format("%.0f %.0f", positionX, positionY));
-        telemetry.addData("arm", String.format("%s %.2f %d %s", onArmReset, armMotor.getPower(), armMotor.getCurrentPosition(), armMotor.getMode().toString()));
+        telemetry.addData("pos", String.format("%.2f %.0f %.0f",drive_power,  positionX, positionY));
+        //telemetry.addData("arm", String.format("%s %.2f %d %s", onArmReset, armMotor.getPower(), armMotor.getCurrentPosition(), armMotor.getMode().toString()));
+
     }
 }
