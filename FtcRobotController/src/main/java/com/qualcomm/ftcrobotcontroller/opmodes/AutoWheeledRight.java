65d8e6356df1b33f76c9b29ba9e8f60d316dc779
Juan C. Santamaria <jcsantamaria@trimble.com>
2016-02-05 20:04:46 -0500

    Sets target waypoint to the right

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutoWheeledRight.java
@@ -0,0 +1,14 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+/**
+ * AutoWheeled with target beacon on the right side.
+ */
+public class AutoWheeledRight extends AutoWheeled {
+
+    public AutoWheeledRight() {
+        // parameters for waypoint beacon
+        targetxBeacon           =  6700;
+        targetyBeacon           =  7100;
+        targetHeadingBeacon     =    90;
+    }
+}
