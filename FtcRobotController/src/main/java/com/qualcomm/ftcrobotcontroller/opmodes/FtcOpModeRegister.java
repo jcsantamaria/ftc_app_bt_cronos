4eca0f305aa498dfa68c643f275c61e6ef0e3db2
jcmachete <jcsantamaria@trimble.com>
2016-02-08 08:46:19 -0500

    Rename opmodes to red/blue

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -98,8 +98,8 @@ public class FtcOpModeRegister implements OpModeRegister {
     //manager.register("RickyMode", FeelsSensor.class);
     //manager.register("DualDrive", DualMotorDrive.class);
     manager.register("WheeledFullMode", WheeledFullMode.class);
-    manager.register("AutoMode Left", AutoWheeledLeft.class);
-    manager.register("AutoMode Right", AutoWheeledRight.class);
+    manager.register("AutoMode Red", AutoWheeledLeft.class);
+    manager.register("AutoMode Blue", AutoWheeledRight.class);
     manager.register("EncoderCalibrate", EncoderCalibrate.class);
     //manager.register("AutoParkLeft", AutoParkLeft.class);
     //manager.register("AutoParkRight", AutoParkRight.class);
